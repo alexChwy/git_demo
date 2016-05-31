2f3deaa01b7ea62d0c64aabb4bbfd23894624012
alexChwy <chwywsy@sina.com>
2016-05-31 12:34:24 +0800

    initial

---
+++ src/com/yaowang/lansha/tv/action/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.yaowang.lansha.tv.action;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
